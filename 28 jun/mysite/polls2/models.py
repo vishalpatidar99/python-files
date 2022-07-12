23503ec31458a40cbb16f35cecb26a03d9a0761e
Vishal Patidar <vishupatidar10@gmail.com>
2022-07-12 10:42:06 +0530

    11 july

--- 28 jun/mysite/polls2/models.py
+++ 28 jun/mysite/polls2/models.py
@@ -1,3 +1,4 @@
+from locale import T_FMT, T_FMT_AMPM
 from django.db import models
 from datetime import date
 
@@ -30,3 +31,36 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.headline
+
+class City(models.Model):
+    name = models.CharField(max_length=35)
+
+class Person(models.Model):
+    hometown = models.ForeignKey(City, on_delete=models.SET_NULL, blank=True, null=True)
+
+class Book(models.Model):
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+class Topping(models.Model):
+    name = models.CharField(max_length=25)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=25)
+    toppings = models.ManyToManyField(Topping)
+    vegetarian = models.BooleanField(null=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, ", ".join(topping.name for topping in self.toppings.all()),)
+
+class Restaurant(models.Model):
+    pizzas = models.ManyToManyField(Pizza, related_name='restaurants')
+    best_pizza = models.ForeignKey(Pizza, related_name='championed_by', on_delete=models.CASCADE)
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=35, unique=True)
+
+class Book2(models.Model):
+    title = models.CharField(max_length=256)
+    chapters = models.ManyToManyField(Chapter)
+
