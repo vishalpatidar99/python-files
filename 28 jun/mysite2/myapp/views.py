5e92e5547c14dbb3614b623f12fe1009a4c48810
Vishal Patidar <vishupatidar10@gmail.com>
2022-07-20 19:59:46 +0530

    20

--- 28 jun/mysite2/myapp/views.py
+++ 28 jun/mysite2/myapp/views.py
@@ -3,10 +3,24 @@ from .models import*
 from .forms import*
 # Create your views here.
 def form(request):
-    fm = CandidateForm()
-    fm2 = JobForm()
-    fm3 = ApplicationForm()
-    fm4 = QualificationForm()
-    fm5 = InterviewUpdateForm()
-    return render(request, 'form.html', {'form':fm,'form2':fm2,'form3':fm3,'form4':fm4,'form5':fm5})
+    if request.method=="POST":
+        fm = CandidateForm(request.POST)
+        output=''
+        if fm.is_valid():
+            name = fm.cleaned_data['name']
+            email = fm.cleaned_data['email']
+            phone1 = fm.cleaned_data['phone1']
+            phone2 = fm.cleaned_data['phone2']
+            address = fm.cleaned_data['address']
+            gender = fm.cleaned_data['gender']
+            profile = fm.cleaned_data['profile']
+            source = fm.cleaned_data['source']
+            f = Candidate(name=name, email=email,phone1=phone1,phone2=phone2,address=address,gender=gender,profile=profile,source=source)
+            f.save()
+            output = 'Form Was Submitted Successfully...'
+        return render(request, 'form.html', {'form':fm,'output':output})
+
+    else:
+        fm = CandidateForm()
+        return render(request, 'form.html',{'form':fm,'output':''} )
             
